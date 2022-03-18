37fb1167316cb6411b273ca5a73a5ace4adf9f73
Alex Glaubitz <e.pirazzi@gmail.com>
2022-03-18 11:07:57 -0400

    fetch request versatility

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,13 @@
-const getUserRepos = function() {
-  fetch('https://api.github.com/users/octocat')
+const getUserRepos = function(user) {
+  //format our github api url
+  const apiUrl = `https://api.github.com/users/${user}/repos`
+
+  //fetch request to url
+  fetch(apiUrl).then(function(response) {
+    response.json().then(function(data) {
+      console.log(data)
+    })
+  })
 }
 
-getUserRepos();
+getUserRepos('qlaub');
